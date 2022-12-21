3cfa4cd1eaf661603a3bc6bace6263163d20a436
korotkovak <kristinakorotkova059@gmail.com>
2022-12-21 21:11:36 +0800

    Created a background

--- LayoutAuthorizationScreen/Sources/ViewController.swift
+++ LayoutAuthorizationScreen/Sources/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    private lazy var imageBackground: UIImageView = {
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        let imageView = UIImageView(frame: CGRect(x:0, y:0, width: width, height: height))
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .top
+        imageView.center = view.center
+        return imageView
+    }()
+
+    // MARK: - Outlets
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupHierarchy()
     }
 
+    // MARK: - Setup
+
+    private func setupHierarchy() {
+        view.addSubviews([imageBackground])
+
+
+    }
 
 }
+
+extension UIView {
+    func addSubviews(_ subviews: [UIView]) {
+        subviews.forEach { addSubview($0) }
+    }
+}
+
 
